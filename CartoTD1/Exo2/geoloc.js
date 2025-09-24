52511a7807a0a1b27dad81f6ba6d32b942200c5c
xDraawwScript <xdraaww.pro@gmail.com>
2025-09-24 14:38:12 +0200

    rajout css + fetch nom departements

--- CartoTD1/Exo2/geoloc.js
+++ CartoTD1/Exo2/geoloc.js
@@ -13,6 +13,8 @@ const niceLon = 7.2663;
 const marseilleLat = 43.2965;
 const marseilleLon = 5.3698; 
 
+let mapn = 0;
+
 function success(pos) {
     var crd = pos.coords;
 
@@ -52,38 +54,50 @@ function success(pos) {
 
     markerloc.bindPopup(`On est à ${dist} km de marseille`).openPopup();
 
+    document.getElementById("btnTriangle").addEventListener('click', ()=> {
+        map.setView([miamiLat,miamiLon],13);
+    });
+    fetch('https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/arrondissements-version-simplifiee.geojson')
+            .then(response => response.json())
+            .then(data => {
+                L.geoJSON(data, {style: function (feature) {return {
+                            color: 'blue',opacity: 0.2};
+                    },
+                    onEachFeature: function (feature, layer, code) {
+                            layer.bindPopup(feature.properties.nom,feature.properties.code);
+                        }
+                }).addTo(map);
+            })
+            .catch(error => console.error('erreur lors du chargement du GeoJSON :', error));
+
 
     document.getElementById('btnChange').addEventListener('click', () => {
+    if(mapn==0){
     map.removeLayer(tileLayer); 
     tileLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}.png', {
         attribution: 'Map tiles by <a href="http://stamen.com/">Stamen Design</a>',
         maxZoom: 20
     }).addTo(map);
+    mapn=+1;
+    } else {
+    mapn=mapn-1;
+    map.removeLayer(tileLayer);
+    tileLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
+}
     map.setView(map.getCenter(), map.getZoom());
 });
-}
+}   
+
+navigator.geolocation.getCurrentPosition(success, error);
 
 function error(err) {
     console.warn(`ERREUR (${err.code}): ${err.message}`);
 }
-var geojsonUrl = 'https://github.com/gregoiredavid/france-geojson/blob/master/arrondissements.geojson';
 
-
-fetch(geojsonUrl)
-    .then(response => response.json())
-    .then(data => {
-        L.geoJSON(data, {
-            style: { color: 'blue', weight: 2, fillOpacity: 0.1 },
-            onEachFeature: function(feature, layer) {
-                if(feature.properties && feature.properties.nom) {
-                    layer.bindPopup(feature.properties.nom);
-                }
-            }
-        }).addTo(map);
-    })
-    .catch(err => console.error("Erreur GeoJSON :", err));
-navigator.geolocation.getCurrentPosition(success, error);
 
 //Generé
 function distanceHaversine(lat1, lon1, lat2, lon2) {
