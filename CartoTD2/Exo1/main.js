7da35d95bd7d13310a9c999c0f8dd70e3aadb269
xDraawwScript <xdraaww.pro@gmail.com>
2025-09-30 10:35:26 +0200

    correctif lien

--- CartoTD2/Exo1/main.js
+++ CartoTD2/Exo1/main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.157.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.157.0/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 {
