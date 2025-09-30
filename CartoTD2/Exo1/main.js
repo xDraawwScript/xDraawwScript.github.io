c2ffe8b5ede4c03353807c85c1f361a6cdbe41a5
xDraawwScript <xdraaww.pro@gmail.com>
2025-09-30 10:44:10 +0200

    test correctif

--- CartoTD2/Exo1/main.js
+++ CartoTD2/Exo1/main.js
@@ -1,5 +1,6 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.157.0/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.157.0/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 
 const scene = new THREE.Scene();
 {
