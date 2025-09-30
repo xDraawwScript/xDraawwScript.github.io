20a1d5a621616ed3bcf6a3cacf7ce46e555a110c
xDraawwScript <xdraaww.pro@gmail.com>
2025-09-30 19:34:50 +0200

    correctif pour github.io

--- CartoTD2/Exo2/main.js
+++ CartoTD2/Exo2/main.js
@@ -1,6 +1,3 @@
-import * as BABYLON from '@babylonjs/core';
-import '@babylonjs/loaders';
-
 const canvas = document.getElementById('renderCanvas');
 const engine = new BABYLON.Engine(canvas, true);
 const scene = new BABYLON.Scene(engine);
