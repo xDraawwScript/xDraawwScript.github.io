a03e90881e151dc2a119af0b7f452f8fba6a7adc
xDraawwScript <xdraaww.pro@gmail.com>
2025-10-07 10:26:28 +0200

    globe avec drapeau fonctionnel

---
+++ CartoTD3/Exo1/main.js
@@ -0,0 +1,91 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const light = new THREE.PointLight(0xffffff, 1);
+light.position.set(5, 3,5);
+scene.add(light);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+const textureLoader = new THREE.TextureLoader();
+const earthTexture = textureLoader.load('https://threejs.org/examples/textures/planets/earth_atmos_2048.jpg');
+
+const geometry = new THREE.SphereGeometry(1, 64, 64);
+const material = new THREE.MeshPhongMaterial({ map: earthTexture });
+const earth = new THREE.Mesh(geometry, material);
+scene.add(earth);
+
+camera.position.z = 3;
+
+const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.05;
+    controls.rotateSpeed = 0.8;
+    controls.zoomSpeed = 0.6;
+    controls.enablePan = false; 
+    controls.minDistance = 1.5;
+    controls.maxDistance = 6;
+
+function latLonToCartesian(lat, lon, radius = 1) {
+    const phi = THREE.MathUtils.degToRad(90 - lat);
+    const theta = THREE.MathUtils.degToRad(lon + 180);
+    const x = -radius * Math.sin(phi) * Math.cos(theta);
+    const y = radius * Math.cos(phi);
+    const z = radius * Math.sin(phi) * Math.sin(theta);
+    return new THREE.Vector3(x, y, z);
+}
+
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition((position) => {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    
+    const markerPos = latLonToCartesian(lat, lon, 1.02);
+    const markerGeo = new THREE.SphereGeometry(0.02, 8, 8);
+    const markerMat = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const marker = new THREE.Mesh(markerGeo, markerMat);
+    marker.position.copy(markerPos);
+    scene.add(marker);
+    }
+);
+}else{
+    console.error("geoloc non supportée");
+}
+function addFlag(lat, lon, flagUrl, countryName) {
+  const pos = latLonToCartesian(lat, lon, 1.02);
+
+  const flagTexture = new THREE.TextureLoader().load(flagUrl);
+  const flagGeo = new THREE.PlaneGeometry(0.1, 0.04);
+  const flagMat = new THREE.MeshBasicMaterial({map: flagTexture,side: THREE.DoubleSide,transparent: true});
+
+  const flag =new THREE.Mesh(flagGeo, flagMat);
+  flag.position.copy(pos);
+  scene.add(flag);
+}
+async function loadCountries(){
+    const response = await fetch('https://restcountries.com/v3.1/all?fields=name,latlng,flags')
+    const countries = await response.json();
+    countries.forEach(country => {
+        const latlng = country.latlng;   
+        const flagUrl = country.flags.png;
+        const name = country.name.common;
+        if (latlng && flagUrl) {
+          addFlag(latlng[0], latlng[1], flagUrl, name);
+        }
+    });
+}
+loadCountries();
+function animate() {
+    controls.update()
+    renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
+
